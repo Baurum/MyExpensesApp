592839283acc4f7adef482510275c5349a63ed17
Marcos <baurumtec@gmail.com>
2017-04-25 11:03:52 +0200

    Add Splash Activity Add new App Icon

--- app/src/main/java/bau/com/myexpensesapp/SplashActivity.java
+++ app/src/main/java/bau/com/myexpensesapp/SplashActivity.java
@@ -1,13 +1,39 @@
 package bau.com.myexpensesapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/***************************************************************************************************
+ * Splash Activity
+ **************************************************************************************************/
 public class SplashActivity extends AppCompatActivity {
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mContext = this;
+        splashTime();
+    }
+
+    /***********************************************************************************************
+     * Method to init the second activity with a delay
+     **********************************************************************************************/
+    private void splashTime() {
+        // Move on to the next activity in SPLASH_TIME_OUT seconds
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(mContext, ExpenseActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
+
